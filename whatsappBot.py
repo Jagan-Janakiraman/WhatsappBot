df4efc109d93e7c7e616f89169b0f7b71ad116f7
jagan-jrc <jaganjrc@gmail.com>
2023-06-25 18:22:00 +0530

    updated code commits

--- whatsappBot.py
+++ whatsappBot.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 
 # Fn to send a message to user
 def user_chat(name_user):
+    # change the xpath by coping the xpath form inspect tab of that compotent if its not working
     user_chat = browser.find_element_by_xpath('//div[@class="_3u328 copyable-text-selectable-text')
     user_chat.send_keys(name_user)
 
@@ -25,11 +26,13 @@ for name_user in name_list:
         user.click()
 
         # Enter the message in the input box
+        # change the xpath by coping the xpath form inspect tab of that compotent if its not working
         box_message = browser.find_element_by_xpath('//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[1]/p')
         box_message.send_keys("Hi jagan Here ... using the WhatsApp bot")
         time.sleep(10)
 
         # Click the send button
+        # change the xpath by coping the xpath form inspect tab of that compotent if its not working
         box_message = browser.find_element_by_xpath('//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[2]/button/span')
         box_message.click()
         time.sleep(20)
