25dfff36c3d36349a9a2af731b36753008084436
jagan-jrc <jaganjrc@gmail.com>
2023-06-29 21:15:30 +0530

    updated code

--- whatsappBot.py
+++ whatsappBot.py
@@ -1,17 +1,16 @@
 import time
 from selenium import webdriver
 
-# Fn to send a message to user
-def user_chat(name_user):
-    # change the xpath by coping the xpath form inspect tab of that compotent if its not working
+# Function to send a message to a user in WhatsApp
+def open_chat(recipient_name):
     user_chat = browser.find_element_by_xpath('//div[@class="_3u328 copyable-text-selectable-text')
-    user_chat.send_keys(name_user)
+    user_chat.send_keys(recipient_name)
 
-# Seting up the Chrome WebDriver
+# Set up the Chrome WebDriver
 my_options = webdriver.ChromeOptions()
 browser = webdriver.Chrome('chromedriver.exe', options=my_options)
 
-# Opening WhatsApp Web in chrome browser
+# Open WhatsApp Web
 browser.get('https://web.whatsapp.com')
 time.sleep(20)
 
@@ -19,25 +18,19 @@ time.sleep(20)
 name_list = ['Appa']
 
 # Iterate over each name in the list
-for name_user in name_list:
+for recipient_name in name_list:
     try:
         # Find the user in the chat list and click on it
-        user = browser.find_element_by_xpath('//span[@title="{}"]'.format(name_user))
+        user = browser.find_element_by_xpath('//span[@title="{}"]'.format(recipient_name))
         user.click()
 
         # Enter the message in the input box
-        # change the xpath by coping the xpath form inspect tab of that compotent if its not working
         box_message = browser.find_element_by_xpath('//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[1]/p')
         box_message.send_keys("Hi jagan Here ... using the WhatsApp bot")
-        time.sleep(10)
 
         # Click the send button
-        # change the xpath by coping the xpath form inspect tab of that compotent if its not working
         box_message = browser.find_element_by_xpath('//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[2]/button/span')
         box_message.click()
-        time.sleep(20)
-
+        
     except Exception as e:
         print(e)
-# closing the browser
-browser.close()
